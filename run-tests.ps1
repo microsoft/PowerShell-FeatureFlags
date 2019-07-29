df7c3a08ad41f9e26f5f3d6c5ffbe6413d9c75b7
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2019-07-29 16:35:57 +0100

    List files in External before testing
    Still trying to understand what's wrong with Linux builds.

--- run-tests.ps1
+++ run-tests.ps1
@@ -1,5 +1,9 @@
 # Mostly for use of CI/CD. Install Pester and run tests.
 
+# TODO: remove.
+# List dependencies before testing. Troubleshooting a build failure on Linux.
+Get-ChildItem -Recurse -Path "$PSScriptRoot/External" -Depth 1 | ForEach-Object {Write-Host $_.FullName}
+
 Install-Module Pester -Force -Scope CurrentUser
 $FailedTests = Invoke-Pester -EnableExit
 if ($FailedTests -gt 0) {
