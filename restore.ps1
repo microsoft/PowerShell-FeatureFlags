edd1fc0fc10243ecc1923991806c87f0b10591eb
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2019-07-29 15:37:19 +0100

    List all restored files for PowerShell Core builds
    Attempting to troubleshoot failed builds on Linux.

--- restore.ps1
+++ restore.ps1
@@ -1,5 +1,6 @@
 param (
-    [switch] $Clean
+    [switch] $Clean,
+    [switch] $List = $false
 )
 $outputDir = "External"
 
@@ -10,5 +11,10 @@ if ($Clean) {
 
     if (-not $?) {
         Write-Error "Could not install packages." -ErrorAction Stop
+    }
+
+    if ($List) {
+        # List all files for troubleshooting purposes.
+        Get-ChildItem -Recurse $outputDir | ForEach-Object { Write-Host $_.FullName }
     }
 }
