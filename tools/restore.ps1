1980f43b662619e39902371a5396852e73906a3c
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2019-07-29 17:26:22 +0100

    Move tools for ci/cd in the tools directory

--- tools/restore.ps1
+++ tools/restore.ps1
@@ -2,12 +2,13 @@ param (
     [switch] $Clean,
     [switch] $List = $false
 )
-$outputDir = "External"
+$parentDir = Split-Path -Parent (Split-Path -Parent $PSCommandPath)
+$outputDir = Join-Path $parentDir -ChildPath "External"
 
 if ($Clean) {
     Remove-Item -Path $outputDir -Recurse -Force
 } else {
-    dotnet restore --packages $outputDir --force 
+    dotnet restore $parentDir --packages $outputDir --force 
 
     if (-not $?) {
         Write-Error "Could not install packages." -ErrorAction Stop
--- tools/run-tests.ps1
+++ tools/run-tests.ps1
@@ -1,7 +1,8 @@
 # Mostly for use of CI/CD. Install Pester and run tests.
+$parentDir = Split-Path -Parent (Split-Path -Parent $PSCommandPath)
 
 Install-Module Pester -Force -Scope CurrentUser
-$FailedTests = Invoke-Pester -EnableExit
+$FailedTests = Invoke-Pester $parentDir -EnableExit
 if ($FailedTests -gt 0) {
     Write-Error "Error: $FailedTests Pester tests failed."
     exit $FailedTests
