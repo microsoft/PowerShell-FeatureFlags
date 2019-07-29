a7489942e676e7f725963e16eafa20a7a556de70
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2019-07-29 16:42:48 +0100

    Remove debug prints in run-tests.ps1

--- run-tests.ps1
+++ run-tests.ps1
@@ -1,9 +1,5 @@
 # Mostly for use of CI/CD. Install Pester and run tests.
 
-# TODO: remove.
-# List dependencies before testing. Troubleshooting a build failure on Linux.
-Get-ChildItem -Recurse -Path "$PSScriptRoot/External" | ForEach-Object {Write-Host $_.FullName}
-
 Install-Module Pester -Force -Scope CurrentUser
 $FailedTests = Invoke-Pester -EnableExit
 if ($FailedTests -gt 0) {
