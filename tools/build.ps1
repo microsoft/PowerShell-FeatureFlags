238c654d6aa2a910ac56ae50f5600c0db6095d17
Andrea Spadaccini <andrea.spadaccini@microsoft.com>
2019-07-30 14:29:42 +0100

    Add a very simple "build" script

---
+++ tools/build.ps1
@@ -0,0 +1,33 @@
+$ErrorActionPreference = "Stop"
+
+$baseDir = Split-Path -Parent (Split-Path -Parent $PSCommandPath)
+$buildDir = Join-Path $baseDir -ChildPath "bld"
+
+if (-not (Test-Path $baseDir/External/newtonsoft.json) -or -not (Test-Path $baseDir/External/njsonschema)) {
+    Write-Error "Missing dependencies. Please run restore.ps1."
+}
+
+if (Test-Path $buildDir) {
+    Write-Host "Cleaning up directory $buildDir"
+    Remove-Item $buildDir -Force -Recurse
+}
+
+Write-Host "Creating $buildDir and copying everything in there"
+$null = New-Item -Path $buildDir -ItemType Directory -Force
+$null = New-Item -Path $buildDir/External -ItemType Directory -Force
+
+Copy-Item -Force $baseDir/FeatureFlags.ps?1 $buildDir
+Copy-Item -Force $baseDir/featureflags.schema.json $buildDir
+Copy-Item -Force $baseDir/LICENSE $buildDir
+Copy-Item -Force $baseDir/README.md $buildDir
+Copy-Item -Force $baseDir/External/newtonsoft.json $buildDir/External -Recurse
+Copy-Item -Force $baseDir/External/njsonschema $buildDir/External -Recurse
+
+$nupkgFiles = Get-ChildItem $buildDir -Recurse | Where-Object {$_.Extension -ieq ".nupkg"}
+if ($nupkgFiles.Count -gt 0) {
+    Write-Host "Removing $($nupkgFiles.Count) nupkg files: $nupkgFiles"
+    $nupkgFiles | ForEach-Object {Remove-Item $_.FullName}
+}
+
+Write-Host "List of files (depth == 2):"
+Get-ChildItem $buildDir -Depth 2 | ForEach-Object { Write-Host $_.FullName }
