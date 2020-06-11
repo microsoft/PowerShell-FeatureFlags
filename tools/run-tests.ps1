b825c6520f39824cc402dff5d5a5f3e23cd34947
Andrea Spadaccini <andreas@microsoft.com>
2020-06-11 11:38:03 +0100

    Pin Pester version to 4.x
    Something changed in Pester 5 and this is causing issues in CI.

--- tools/run-tests.ps1
+++ tools/run-tests.ps1
@@ -2,11 +2,18 @@
 $parentDir = Split-Path -Parent (Split-Path -Parent $PSCommandPath)
 $testDir = Join-Path $parentDir -ChildPath "test"
 
+# Debug info.
+$PSVersionTable | Out-String
+
+Write-Host "Checking for Pester > 4.0.0..."
 $pester = Get-Module -ListAvailable | Where-Object {$_.Name -eq "Pester" -and $_.Version -gt '4.0.0'}
 if ($pester.Count -eq 0) {
     Write-Host "Cannot find the Pester module. Installing it."
-    Install-Module Pester -Force -Scope CurrentUser
+    Install-Module Pester -Force -Scope CurrentUser -RequiredVersion 4.10.1
+} else {
+    Write-Host "Found Pester version $($pester.Version)."
 }
+
 $FailedTests = Invoke-Pester $testDir -EnableExit -OutputFile "test/results.xml" -OutputFormat "NUnitXML" -CodeCoverage "$parentDir/FeatureFlags.psm1"
 if ($FailedTests -gt 0) {
     Write-Error "Error: $FailedTests Pester tests failed."
