71cdf3cf2669a03a45772faa26cb21ddb2c93665
Andrea Spadaccini <andrea.spadaccini@microsoft.com>
2020-12-25 22:24:12 +0000

    fix: port tests to Pester 5 syntax
    Also chante run-tests.ps1 so it installs Pester 5.1.0.

--- tools/run-tests.ps1
+++ tools/run-tests.ps1
@@ -1,8 +1,8 @@
 # Mostly for use of CI/CD. Install Pester and run tests.
 param (
-  # Set to true to remove all Pester versions and install 4.10.1 which supports
+  # Set to true to remove all Pester versions and install 5.1.0
   # the way unit tests are written.
-  [switch] $CleanPesterAndInstallV4 = $false
+  [switch] $CleanPesterAndInstallV5 = $false
 )
 
 $parentDir = Split-Path -Parent (Split-Path -Parent $PSCommandPath)
@@ -16,14 +16,14 @@ $pesterVersions = Get-Module -ListAvailable | Where-Object {$_.Name -eq "Pester"
 $pesterVersions | % { Write-Host $_.Name $_.Version }
 
 if ($pesterVersions.Count -eq 0) {
-   Write-Warning "No Pester found, will install Pester 4.10.1"
-   $CleanPesterAndInstallV4 = $true
+   Write-Warning "No Pester found, will install Pester 5.1.0"
+   $CleanPesterAndInstallV5 = $true
 }
 
-if ($CleanPesterAndInstallV4) {
+if ($CleanPesterAndInstallV5) {
    Remove-Module Pester -Force
    Uninstall-Module Pester -Force -AllVersions
-   Install-Module Pester -Force -Scope CurrentUser -RequiredVersion 4.10.1
+   Install-Module Pester -Force -Scope CurrentUser -RequiredVersion 5.1.0
 }
 
 $FailedTests = Invoke-Pester $testDir -EnableExit -OutputFile "test/results.xml" -OutputFormat "NUnitXML" -CodeCoverage "$parentDir/FeatureFlags.psm1"
